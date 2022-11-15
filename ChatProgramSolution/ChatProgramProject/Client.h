9b3e79fc6ea24c91a069449a7e5ad2ab18dfbead
BobbyDMullinsIII <bobbymullins20000@gmail.com>
2022-11-15 15:51:14 -0500

    Removed unnecessary includes

--- ChatProgramSolution/ChatProgramProject/Client.h
+++ ChatProgramSolution/ChatProgramProject/Client.h
@@ -3,14 +3,9 @@
 //Includes taken from example client.cc file
 #include <stdlib.h>
 //#include <unistd.h>
-#include <sys/types.h>
-//#include <sys/socket.h>
-//#include <netinet/in.h>
-//#include <arpa/inet.h>
 //#include <netdb.h>
 #include <iostream>
 #include <string.h>
-//#include <strings.h>
 
 //Normal includes
 #include <string>
--- ChatProgramSolution/ChatProgramProject/Server.h
+++ ChatProgramSolution/ChatProgramProject/Server.h
@@ -3,13 +3,9 @@
 //Includes taken from example server.cc file
 #include <stdlib.h>
 //#include <unistd.h>
-#include <sys/types.h>
-//#include <sys/socket.h>
-//#include <netinet/in.h>
 //#include <netdb.h>
 #include <iostream>
 #include <string.h>
-#include <stdio.h>
 
 //Normal includes
 #include <string>
